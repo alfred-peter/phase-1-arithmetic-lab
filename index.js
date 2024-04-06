f661389659cfa956998b89ac9058e317d0b12787
alfred-peter <alfredkilonzo90@gmail.com>
2024-04-06 11:45:54 +0300

    completed labs

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+let num3 = 23;
+let num4 = 19;
+let mod = num3 % num4;
+
+
+let numbers = [15,16,17,18,19,20];
+let max = Math.max(...numbers);
